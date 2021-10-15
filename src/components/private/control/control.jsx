bed9f7ee02477524b5685120a66c15f7da8e7a71
user <y.dubovitsky@gmail.com>
2021-10-15 15:00:31 +0300

    added stat component

--- src/components/private/control/control.jsx
+++ src/components/private/control/control.jsx
@@ -1,11 +1,21 @@
 import ProtectedRoute from '../../../wrappers/protected-route';
 import PostForm from '../post-form/post-form';
+import Stat from '../stat/stat';
 import style from './control.module.css';
 
 export default function Control() {
   return (
     <div className={style.container}>
       <ProtectedRoute path="/admin/blog" component={PostForm} />
+
+      {/* TODO Добавить https://react-dnd.github.io/react-dnd/examples/tutorial */}
+      <div className={style.statList}>
+        <Stat/>
+        <Stat/>
+        <Stat/>
+        <Stat/>
+      </div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem magnam officia at iste esse! Facilis qui est repellat quasi eaque?</p>
     </div>
   )
 }
---
+++ src/components/private/stat/stat.jsx
@@ -0,0 +1,8 @@
+import style from './stat.module.css';
+
+export default function Stat() {
+  return (
+    <div className={style.container}>
+    </div>
+  )
+}
