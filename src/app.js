384e4ea53ec2ba07990ee90000ee8c5c0710a7b2
user <y.dubovitsky@gmail.com>
2021-12-02 12:28:22 +0300

    redesigned the main Route in the application

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 import cn from 'classnames';
-import { Switch, Route, Redirect } from 'react-router';
-import ErrorPage from './components/common/error-page/error-page';
-import ProtectedRoute from './wrappers/protected-route';
+import { Redirect, Route, Switch } from 'react-router';
 import style from './app.module.css';
-import HomeLayout from './layouts/home-layout/home-layout';
+import ErrorPage from './components/common/error-page/error-page';
 import AdminLayout from './layouts/admin-layout/admin-layout';
+import HomeLayout from './layouts/home-layout/home-layout';
 import UserProfileLayout from './layouts/user-profile-layout/user-profile-layout';
+import ProtectedRoute from './wrappers/protected-route';
 
 function App(props) {
 
   return (
     <div className={cn(style.container)}>
       <Switch>
-        <Route path="/" exact={true} component={HomeLayout} />
-        {/* TODO Добавить в адресную строку ссылку на текущего пользователя! */}
+        {/* TODO Что то тут не так */}
+        <Route path="/home" component={HomeLayout} />
         <Route path="/user-profile" component={UserProfileLayout} />
         <ProtectedRoute path="/admin" component={AdminLayout} />
         <Route path="/error-page" component={ErrorPage} />
-        <Redirect to="/" />
+        <Redirect to="/home" />
       </Switch>
     </div>
   );
