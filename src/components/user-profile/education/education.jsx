21f9a67c4fbeb35b61820f0dccf9fa43fdd3c285
user <y.dubovitsky@gmail.com>
2022-01-11 16:56:21 +0300

    now the education component receives data from the backend

--- src/components/user-profile/education/education.jsx
+++ src/components/user-profile/education/education.jsx
@@ -1,26 +1,44 @@
 import cn from 'classnames';
 
-import style from './education.module.css';
+import styles from './education.module.css';
+import { useSelector } from 'react-redux';
+import { selectUserEntity, selectUser } from '../../../redux/features/user/userSlice';
+import Loader from '../../common/loader/loader.component';
 
+const Education = () => {
 
-export default function Education() {
+  const { universities } = useSelector(selectUserEntity);
+  const { status } = useSelector(selectUser);
+
+  if (status === 'idle') {
+    return <Loader />;
+  }
+
+  const showUserUniversities = (universities) => {
+    return universities.map(university => (
+      <div key={university.id} className={cn(styles.row)}>
+        <div className={styles.universityName}>{university.name}</div>
+        <div className={styles.speciality}>{university.speciality}</div>
+        <div className={styles.cathedra}>{university.cathedra}</div>
+        <div className={styles.duration}>
+          {university.startYear} - {university.endYear}
+        </div>
+        <div className={styles.description}>{university.description}</div>
+      </div>
+    ))
+  }
+
   return (
-    <div className={cn(style.container)}>
-      <div className={cn(style.info)}>
+    <div className={cn(styles.container)}>
+      <div className={cn(styles.info)}>
         <span>Education</span>
         <h2>Education</h2>
       </div>
-      <div className={cn(style.table)}>
-        <div className={cn(style.row)}>
-          Lorem ipsum dolor sit amet.
-        </div>
-        <div className={cn(style.row)}>
-          Lorem ipsum dolor sit amet.
-        </div>
-        <div className={cn(style.row)}>
-          Lorem ipsum dolor sit amet.
-        </div>
+      <div className={cn(styles.table)}>
+        {showUserUniversities(universities)}
       </div>
     </div>
   )
 }
+
+export default Education;
