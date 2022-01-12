411787b91460e159329991b062e7c904f4872f82
user <y.dubovitsky@gmail.com>
2022-01-12 11:12:52 +0300

    recent-works component fetches data from backend

--- src/components/user-profile/recent-works/recents-works.jsx
+++ src/components/user-profile/recent-works/recents-works.jsx
@@ -1,46 +1,59 @@
 import cn from 'classnames';
-
-import style from './recents-works.module.css';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import image from '../../../assets/github.png';
+import { selectUser } from '../../../redux/features/user/userSlice';
+import Loader from '../../common/loader/loader.component';
+import styles from './recents-works.module.css';
 
-export default function RecentWorks() {
+const RecentWorks = () => {
+
+  const { status, userEntity } = useSelector(selectUser);
+  const [countOfWorks, setСountOfWorks] = useState(10);
+
+  const showRecentUserWork = (works, countOfWorks = 6) => {
+    return works.slice(0, countOfWorks).map(work => {
+      const { id, name, html_url } = work;
+
+      return <div key={id} className={cn(styles.workContainer)}>
+        <div className={cn(styles.work)}>
+          <img src={image} alt="" />
+          <div className={styles.repoName}>{name}</div>
+        </div>
+        <div className={cn(styles.workInfo)}>
+          {name}
+          <a href={html_url} target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+        </div>
+      </div>
+    })
+  }
+
+  if (status === 'idle') {
+    return <Loader />;
+  }
 
   return (
-    <div className={cn(style.container)}>
-      <div className={cn(style.info)}>
-        <span className={cn(style.heading)}>MY WORK</span>
-        <h2 className={cn(style.header)}>Recent Works</h2>
+    <div className={cn(styles.container)}>
+      <div className={cn(styles.info)}>
+        <span className={cn(styles.heading)}>MY WORK</span>
+        <h2 className={cn(styles.header)}>Recent Works</h2>
       </div>
-      <div className={cn(style.works)}>
-        <div className={cn(style.workContainer)}>
-          <div className={cn(style.work)}>
-            <img src={image} alt="" />
-          </div>
-          <div className={cn(style.workInfo)}>
-            Lorem, ipsum.
-            <a href="http://" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a> 
-          </div>
-        </div>
-        <div className={cn(style.workContainer)}>
-          <div className={cn(style.work)}>
-            <img src={image} alt="" />
-          </div>
-          <div className={cn(style.workInfo)}>
-            Lorem, ipsum.
-            <a href="http://" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a> 
-          </div>
-        </div>
-        <div className={cn(style.workContainer)}>
-          <div className={cn(style.work)}>
-            <img src={image} alt="" />
-          </div>
-          <div className={cn(style.workInfo)}>
-            Lorem, ipsum.
-            <a href="http://" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a> 
-          </div>
-        </div>
+      <div className={cn(styles.works)}>
+        {
+          userEntity.hasOwnProperty('repos')
+            ?
+            showRecentUserWork(userEntity.repos, countOfWorks)
+            :
+            ''
+        }
       </div>
-      <button>Load More</button>
+      <button
+        onClick={() => setСountOfWorks(countOfWorks + 3)}
+      >
+        Load More
+      </button>
     </div>
   )
-}
+};
+
+export default RecentWorks;
