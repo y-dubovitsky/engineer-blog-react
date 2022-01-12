d9fd99f769f8225bbc767c5b160311e735c4d26e
user <y.dubovitsky@gmail.com>
2022-01-12 11:11:13 +0300

    middlewares added to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,8 @@ import workReducer from './features/work/workSlice';
 import postReducer from './features/post/postSlice';
 import skillReducer from './features/skill/skillSlice';
 import universityReducer from './features/university/universitySlice';
+import fetchUserReposMiddleware from './middleware/fetchUserRepos.middleware';
+import loggingMiddleware from './middleware/logging.middleware';
 
 function configureAppStore() {
   const store = configureStore({
@@ -17,7 +19,13 @@ function configureAppStore() {
       skill: skillReducer,
       university: universityReducer
     },
-  })
+    middleware: (getDefaultMiddleware) => [
+      ...getDefaultMiddleware(),
+      fetchUserReposMiddleware,
+      loggingMiddleware
+    ]
+  },
+  )
   return store;
 }
 
